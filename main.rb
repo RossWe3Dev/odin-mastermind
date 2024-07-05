c685b13fb27a6926526ec0f742e1762e3a68aa8f
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-07-05 10:53:53 +0200

    Refactor code for readability

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 class Game
-  COLORED_PEGS = [:blue, :orange, :green, :purple, :yellow, :pink].freeze
+  COLORED_PEGS = %i[blue orange green purple yellow pink].freeze
   RIGHT_COLOR = :red
-  RIGHT_POSITION_and_COLOR = :white
+  RIGHT_POSITION_AND_COLOR = :white
 
   attr_reader :secret_code, :name
 
@@ -12,16 +12,15 @@ class Game
 
   def player_input
     puts "Please pick 4 pegs from [blue, orange, green, purple, yellow, pink]"
-    input = gets.chomp.split().map{|color| color.to_sym }
-    if input.all? { |chosen_pegs| COLORED_PEGS.include?(chosen_pegs) } && input.length == 4
-      return puts "Good! #{input}"
-    else
+    input = gets.chomp.split.map(&:to_sym)
+    until input.all? { |chosen_pegs| COLORED_PEGS.include?(chosen_pegs) } && input.length == 4
       puts "Invalid #{input}"
       player_input
     end
+    puts "Good! #{input}"
   end
 end
 
-master = Game.new('ross')
+master = Game.new("Odin")
 p master.secret_code
 master.player_input
