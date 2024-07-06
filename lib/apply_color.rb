a6bdb9c1ece86498fdd8438bec6204c2fa273d47
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-07-06 17:27:19 +0200

    Refactor matches_count, include Apply_color module
    Included Apply_color module to Game class and moved its file in lib/
    directory. Used colorize_pegs method on CLI outputs.
    Refactored matches_count to avoid over counting pegs of the same color.
    The new logic creates a copy of guess and each iteration if a match is
    found the corresponding peg is removed.
    
    [Bonus explanation] A guess_copy is necessary because @guess is an
    instance variable available to all methods and even if it is regenerated
    each turn by the play loop, it needs to be passed to the game_over?
    method and therefore cannot be changed by deleting pegs from it.

---
+++ lib/apply_color.rb
@@ -0,0 +1,14 @@
+module Apply_color
+  COLOR_MAP = {
+    blue: :blue,
+    green: :light_green,
+    purple: :magenta,
+    pink: :light_red,
+    orange: :yellow,
+    yellow: :light_yellow
+  }.freeze
+
+  def colorize_pegs(color_symbols_array)
+    color_symbols_array.map { |symbol| symbol.to_s.colorize(COLOR_MAP[symbol] || :default) }
+  end
+end
--- main.rb
+++ main.rb
@@ -1,7 +1,10 @@
 require "colorize"
 
+require_relative "lib/apply_color"
+
 class Game
-  COLORED_PEGS = %i[blue orange green purple yellow pink].freeze
+  include Apply_color
+  COLORED_PEGS = %i[blue purple pink orange yellow green].freeze
 
   attr_reader :secret_code, :name
 
@@ -13,7 +16,7 @@ class Game
 
   def play
     12.times do |attempt|
-      puts "Attempt number #{attempt + 1}".colorize(:magenta)
+      puts "Attempt number #{attempt + 1}".colorize(:cyan)
       player_guess
       display_feedback
 
@@ -26,7 +29,7 @@ class Game
 
   def player_guess
     loop do
-      puts "Please pick 4 colors from [blue, orange, green, purple, yellow, pink]".colorize(:green)
+      puts "Please pick 4 colors from [#{colorize_pegs(COLORED_PEGS).join(' ')}]"
       @guess = gets.chomp.downcase.split.map(&:to_sym)
       return @guess if @guess.all? { |chosen_pegs| COLORED_PEGS.include?(chosen_pegs) } && @guess.length == 4
 
@@ -36,18 +39,26 @@ class Game
 
   def display_feedback
     white_pegs = (0...4).count { |i| @guess[i] == @secret_code[i] }
-
     red_pegs = [(matches_count - white_pegs), 0].max
 
-    print "#{white_pegs} white pegs (correct color and position),".colorize(:white)
+    puts "Your guess: #{colorize_pegs(@guess).join(' ')}"
+    print "#{white_pegs} white pegs (correct color and position),"
     puts " #{red_pegs} red pegs (only correct color)".colorize(:red)
     @guess
   end
 
   def matches_count
     all_matches = 0
+    guess_copy = @guess.dup
+
     @secret_code.each do |secret_peg|
-      all_matches += 1 if @guess.any? { |guess_peg| guess_peg == secret_peg }
+      # Find the index of matching peg, if none (nil) skip to the next iteration
+      index = guess_copy.index(secret_peg)
+      next unless index
+
+      all_matches += 1
+      # Remove the matched peg from the guess copy to avoid overcounting
+      guess_copy.delete_at(index)
     end
     all_matches
   end
@@ -61,7 +72,7 @@ class Game
       puts "Congratulations! You cracked the code!".colorize(:green)
     else
       puts "You ran out of turns and lost :(".colorize(:red)
-      puts "The code was: #{@secret_code.join(', ')}"
+      puts "The code was: #{colorize_pegs(@secret_code).join(' ')}"
     end
   end
 end
